52e2c0b1359ff83aaf33474a166c78b248a4dc64
Luizcs2 <luizohto2012@gmail.com>
2025-04-28 22:06:13 +0100

    hook added

--- client/hooks/useUser.tsx
+++ client/hooks/useUser.tsx
@@ -74,3 +74,10 @@ export const MyUserContextProvider = (props:Props) => {
 };
 
 
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if(context === undefined) {
+        throw new Error('useUser must be used wihtin a myUsercontextProvider')
+    }
+    return context;
+};
